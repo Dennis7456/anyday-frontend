8cc51cd077a41f872581bd2d0902b895ca9b42ee
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2024-06-11 20:48:12 +0300

    build: navigation bar story

---
+++ src/stories/NavigationBar/NavigationBar.ts
@@ -0,0 +1,42 @@
+// import type { Meta, StoryObj } from '@storybook/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { NavigationBar } from './NavigationBar';
+// import UserContext from '../../App';
+
+// const meta = {
+//     title: 'Example/NavigationBar',
+//     component: NavigationBar,
+//     decorators: [
+//         (Story) => (
+//             <MemoryRouter initialEntries= { ['/']} >
+//             <Story />
+//             < /MemoryRouter>
+//     ),
+//   ],
+// parameters: {
+//     layout: 'centered',
+//   },
+// } satisfies Meta<typeof NavigationBar>;
+
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+
+// export const LoggedOut: Story = {
+//     decorators: [
+//         (Story) => (
+//             <UserContext.Provider value= { null} >
+//             <Story />
+//             < /UserContext.Provider>
+//     ),
+//   ],
+// };
+
+// export const LoggedIn: Story = {
+//     decorators: [
+//         (Story) => (
+//             <UserContext.Provider value= {{ name: 'John Doe' }} >
+//     <Story />
+//     < /UserContext.Provider>
+//     ),
+//   ],
+// };
