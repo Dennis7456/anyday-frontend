8183e7879f6146607c1d67f72fa793cb6710c3b0
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2024-06-12 03:17:27 +0300

    test: router error

--- src/App.tsx
+++ src/App.tsx
@@ -47,68 +47,68 @@ const App = () => {
   console.log(user)
   return (
     <div className="App">
-      <Router>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>
-            Essay Writing Service - We Do Essay Writing And Formatting
-          </title>
-          <link rel="canonical" href="http://mysite.com/example" />
-          <meta
-            name="description"
-            content="Professional academic writing services"
-          />
-        </Helmet>
-        {user !== null ? null : (
-          <section className="px-6 py-2 bg-surface">
-            <NavigationBar />
-          </section>
-        )}
-        {/*<section className='px-6 py-8 bg-surface'>
+      {/* <Router> */}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>
+          Essay Writing Service - We Do Essay Writing And Formatting
+        </title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta
+          name="description"
+          content="Professional academic writing services"
+        />
+      </Helmet>
+      {user !== null ? null : (
+        <section className="px-6 py-2 bg-surface">
+          <NavigationBar />
+        </section>
+      )}
+      {/*<section className='px-6 py-8 bg-surface'>
         <NavigationBar /> 
       </section>*/}
 
-        <UserContext.Provider value={user}>
-          <ToastContainer />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <section className="bg-gradient-to-r to-primary from-secondary-container flex justify-around items-center p-10 h-50">
-                  <div className="">
-                    <p className="tracking-wider text-5xl pr-10">
-                      Online Essay Writing Service{' '}
-                    </p>
-                    <p className="text-5xl tracking-wider mt-10">
-                      Available 24/7
-                    </p>
-                    <p className="text-lg py-10">
-                      We do all types of writing at the cheapest rates in town.
-                    </p>
-                  </div>
-                  <div>
-                    <Order />
-                  </div>
-                </section>
-              }
-            ></Route>
-            <Route path="/howitworks" element={<HowItWorks />}></Route>
-            <Route path="/latestreviews" element={<LatestReviews />}></Route>
-            <Route path="/topwriters" element={<TopWriters />}></Route>
-            <Route path="/samples" element={<Samples />}></Route>
-            <Route path="/blog" element={<Blog />}></Route>
-            <Route path="/registration" element={<Registration />}></Route>
-            <Route
-              path="/auth_password_reset"
-              element={<AuthPasswordReset />}
-            ></Route>
-            <Route path="/dashboard" element={<DashBoard />}></Route>
-            <Route path="/settings" element={<Settings />}></Route>
-            <Route path="tables" element={<Tables />}></Route>
-            {/*<Route path='/*' element={ <Navigate to="/dashboard" /> }/>*/}
-          </Routes>
-        </UserContext.Provider>
-      </Router>
+      <UserContext.Provider value={user}>
+        <ToastContainer />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <section className="bg-gradient-to-r to-primary from-secondary-container flex justify-around items-center p-10 h-50">
+                <div className="">
+                  <p className="tracking-wider text-5xl pr-10">
+                    Online Essay Writing Service{' '}
+                  </p>
+                  <p className="text-5xl tracking-wider mt-10">
+                    Available 24/7
+                  </p>
+                  <p className="text-lg py-10">
+                    We do all types of writing at the cheapest rates in town.
+                  </p>
+                </div>
+                <div>
+                  <Order />
+                </div>
+              </section>
+            }
+          ></Route>
+          <Route path="/howitworks" element={<HowItWorks />}></Route>
+          <Route path="/latestreviews" element={<LatestReviews />}></Route>
+          <Route path="/topwriters" element={<TopWriters />}></Route>
+          <Route path="/samples" element={<Samples />}></Route>
+          <Route path="/blog" element={<Blog />}></Route>
+          <Route path="/registration" element={<Registration />}></Route>
+          <Route
+            path="/auth_password_reset"
+            element={<AuthPasswordReset />}
+          ></Route>
+          <Route path="/dashboard" element={<DashBoard />}></Route>
+          <Route path="/settings" element={<Settings />}></Route>
+          <Route path="tables" element={<Tables />}></Route>
+          {/*<Route path='/*' element={ <Navigate to="/dashboard" /> }/>*/}
+        </Routes>
+      </UserContext.Provider>
+      {/* </Router> */}
     </div>
   )
 }
